271618bbfaf795882299ef8b0ca5191a81c837ab
M. Huang <mhuang@nao.cas.cn>
2020-05-26 13:17:18 +0800

    yellow button for dark background; Put `copybutton_prompt_text1 = "... "` in conf.py to add another prompt

--- sphinx_copybutton/__init__.py
+++ sphinx_copybutton/__init__.py
@@ -6,17 +6,28 @@ __version__ = "0.2.12dev0"
 
 logger = logging.getLogger(__name__)
 
+
 def scb_static_path(app):
-    static_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '_static'))
+    static_path = os.path.abspath(os.path.join(
+        os.path.dirname(__file__), '_static'))
     app.config.html_static_path.append(static_path)
+
 
 def add_to_context(app, config):
     # Update the global context
-    config.html_context.update({'copybutton_prompt_text': config.copybutton_prompt_text})
-    config.html_context.update({'copybutton_only_copy_prompt_lines': config.copybutton_only_copy_prompt_lines})
-    config.html_context.update({'copybutton_remove_prompts': config.copybutton_remove_prompts})
-    config.html_context.update({'copybutton_image_path': config.copybutton_image_path})
-    config.html_context.update({'copybutton_selector': config.copybutton_selector})
+    config.html_context.update(
+        {'copybutton_prompt_text': config.copybutton_prompt_text})
+    config.html_context.update(
+        {'copybutton_prompt_text1': config.copybutton_prompt_text1})
+    config.html_context.update(
+        {'copybutton_only_copy_prompt_lines': config.copybutton_only_copy_prompt_lines})
+    config.html_context.update(
+        {'copybutton_remove_prompts': config.copybutton_remove_prompts})
+    config.html_context.update(
+        {'copybutton_image_path': config.copybutton_image_path})
+    config.html_context.update(
+        {'copybutton_selector': config.copybutton_selector})
+
 
 def setup(app):
     logger.verbose('Adding copy buttons to code blocks...')
@@ -25,6 +36,7 @@ def setup(app):
 
     # configuration for this tool
     app.add_config_value("copybutton_prompt_text", "", "html")
+    app.add_config_value("copybutton_prompt_text1", "", "html")
     app.add_config_value("copybutton_only_copy_prompt_lines", True, "html")
     app.add_config_value("copybutton_remove_prompts", True, "html")
     app.add_config_value("copybutton_image_path", "copy-button.svg", "html")
